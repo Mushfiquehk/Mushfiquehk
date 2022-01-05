424e133e1df04772ec69e36fadc7aecc863a8e9b
Mushfiquehk <mushfiquehasankhan@gmail.com>
2022-01-05 01:45:31 -0600

    Redesigning database

---
+++ reup_cart/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReupCartConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'reup_cart'
---
+++ reup_web/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReupWebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'reup_web'
